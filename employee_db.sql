0802ef74b2b91261f5f4fe30509176d6b16dbfc7
Nikhil Sairam Reddy S <snsreddy1988@gmail.com>
2021-08-05 17:36:01 +0530

    Added auto-increment for primary keys

--- employee_db.sql
+++ employee_db.sql
@@ -4,12 +4,12 @@ use empdb;
 
 
 create table department(
-department_id int(10) not null primary key,
+department_id int(10) not null primary key auto_increment,
 department_nm varchar(25) not null
 );
 
 create table employees(
-empid int(10) not null primary key,
+empid int(10) not null primary key auto_increment,
 firstname varchar(45),
 lastname varchar(45),
 dob date,
@@ -19,7 +19,7 @@ foreign key (department_id) references department(department_id)
 );
 
 create table compliance(
-complianceid int(10) not null primary key,
+complianceid int(10) not null primary key auto_increment,
 rltype varchar(15),
 details varchar(250),
 createdate date,
@@ -29,7 +29,7 @@ foreign key (department_id) references department(department_id)
 
 create table statusreport(
 complianceid int(10) not null,
-statusrptid int(10) not null primary key,
+statusrptid int(10) not null primary key auto_increment,
 empid int(10) not null,
 comments varchar(15),
 createdate date,
@@ -40,17 +40,17 @@ foreign key (department_id) references department(department_id)
 );
 
 create table login_master(
-userid int(10) not null primary key,
+userid int(10) not null primary key auto_increment,
 password varchar(30) not null,
 role varchar(20) not null,
 foreign key (userid) references employees(empid)
 );
 
-insert into department(department_id, department_nm) values(1, "top");
-insert into department(department_id, department_nm) values(2, "below");
+insert into department(department_nm) values("top");
+insert into department(department_nm) values("below");
 
-insert into employees(empid, firstname, lastname, dob, email, department_id) values(1, "a", "b", str_to_date(1998-1-1, "%Y-%m-%d"), "p@q.r", 1);
-insert into employees(empid, firstname, lastname, dob, email, department_id) values(2, "c", "d", str_to_date(1998-1-2, "%Y-%m-%d"), "s@t.u", 2);
+insert into employees(firstname, lastname, dob, email, department_id) values("a", "b", str_to_date("1998-1-1", "%Y-%m-%d"), "p@q.r", 1);
+insert into employees(firstname, lastname, dob, email, department_id) values("c", "d", str_to_date("1998-1-2", "%Y-%m-%d"), "s@t.u", 2);
 
-insert into login_master(userid, password, role) values(1, "admin", "admin");
-insert into login_master(userid, password, role) values(2, "emp", "employee");
+insert into login_master(password, role) values("admin", "admin");
+insert into login_master(password, role) values("emp", "user");
